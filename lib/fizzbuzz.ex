081161210ee8335daa0ebb3a58af5d72f57ee9fd
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-30 15:24:39 -0500

    first commit

---
+++ lib/fizzbuzz.ex
@@ -0,0 +1,18 @@
+defmodule Fizzbuzz do
+  @moduledoc """
+  Documentation for `Fizzbuzz`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fizzbuzz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

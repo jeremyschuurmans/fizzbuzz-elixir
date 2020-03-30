9351e1afa1113c9cade98872487d09eff58b63f7
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-30 15:41:48 -0500

    add test and solution for buzz (divisible by 5)

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -5,4 +5,8 @@ defmodule FizzbuzzTest do
   test "it returns 'Fizz' when passed a number divisible by 3" do
     assert Fizzbuzz.fizz(3) == "Fizz"
   end
+
+  test "it returns 'Buzz' when passed a number divisible by 5" do
+    assert Fizzbuzz.fizz(5) == "Buzz"
+  end
 end

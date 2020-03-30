5f652148606b0c66b6ce1846eafc044a600a3f74
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-30 15:53:17 -0500

    add test and implementation for both 3 and 5

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,6 +1,8 @@
 defmodule Fizzbuzz do
   def fizz(number) do
     cond do
+      rem(number, 3) == 0 && rem(number, 5) == 0 ->
+        "FizzBuzz"
       rem(number, 3) == 0 ->
         "Fizz"
       rem(number, 5) == 0 ->
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -9,4 +9,8 @@ defmodule FizzbuzzTest do
   test "it returns 'Buzz' when passed a number divisible by 5" do
     assert Fizzbuzz.fizz(5) == "Buzz"
   end
+
+  test "it returns 'FizzBuzz' when passed a number divisible by both 3 and 5" do
+    assert Fizzbuzz.fizz(15) == "FizzBuzz"
+  end
 end
